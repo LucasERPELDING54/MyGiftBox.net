0bef5cddf77707a238127b3db7953e732dfe0411
Antonin <anto10800@gmail.com>
2023-06-15 12:12:08 +0200

    Connexion

--- gift.appli/src/models/Connexion.php
+++ gift.appli/src/models/Connexion.php
@@ -6,6 +6,8 @@ class Connexion extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'connexion';
     protected $primaryKey = 'id';
+
+    protected $fillable = ['identifiant', 'password'];
     public $timestamps = false;
 
 }
